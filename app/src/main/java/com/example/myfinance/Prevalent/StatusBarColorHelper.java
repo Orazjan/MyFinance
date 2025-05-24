17a817a201897cb082089aacac38330fbbd49588
Orazjan <orazjanov11@gmail.com>
2025-05-24 18:14:57 +0600

    first commit

---
+++ app/src/main/java/com/example/myfinance/Prevalent/StatusBarColorHelper.java
@@ -0,0 +1,23 @@
+package com.example.myfinance.Prevalent;
+
+import android.app.Activity;
+import android.util.TypedValue;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class StatusBarColorHelper {
+
+    public static void setStatusBarColorFromPrimaryVariant(Activity activity) {
+
+        Window window = activity.getWindow();
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        TypedValue typedValue = new TypedValue();
+        activity.getTheme().resolveAttribute(com.google.android.material.R.attr.colorPrimaryVariant, typedValue, true);
+        int colorPrimaryVariant = typedValue.data;
+
+        window.setStatusBarColor(colorPrimaryVariant);
+    }
+}
